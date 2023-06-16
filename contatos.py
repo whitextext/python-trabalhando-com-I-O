404701a50afcfeb946dfe77d1a8893975e10cd95
bruno adriano <brunoadria.contato@gmail.com>
2023-06-16 15:09:58 -0300

    primeiro commit

---
+++ contatos.py
@@ -0,0 +1,5 @@
+class Contatos:
+    def __init__(self, identificador: int, nome: str, email: str) -> None:
+        self.identificador = identificador
+        self.nome = nome
+        self.email = email
